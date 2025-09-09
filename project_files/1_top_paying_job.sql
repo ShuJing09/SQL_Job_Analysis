9f3f79ab3a0eb6a7c682028ebfbf416026291dd6
ShuJing Ding <65424722+ShuJing09@users.noreply.github.com>
2025-09-09 17:37:15 +0800

    Update 1_top_paying_job.sql

--- project_files/1_top_paying_job.sql
+++ project_files/1_top_paying_job.sql
@@ -1,9 +1,9 @@
 /*
 Question: What are the top-paying data analyst jobs?
-- Identify the top 10 highest-paying Data Analyst roles that are available remotely
-- Focuses on job postings with specified salaries (remove nulls)
-- BONUS: Include company names of top 10 roles
-- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
+- Access job_postings_fact for top 10 highest-paying Data Analyst roles that are available remotely
+- Inner join job_postings_fact with company_dim to get company names
+- Remove nulls value in salary_year_avg
+- Why? Offering insights into employment options and location flexibility for Data Analyst.
 */
 
 SELECT	
@@ -23,4 +23,5 @@ WHERE
     salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
+
 LIMIT 10;
