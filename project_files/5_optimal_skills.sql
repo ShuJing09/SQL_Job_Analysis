70fd60cf9bb9c712a1e6c2e2acc26e2493343b00
ShuJing Ding <65424722+ShuJing09@users.noreply.github.com>
2025-09-09 17:49:09 +0800

    Update 5_optimal_skills.sql

--- project_files/5_optimal_skills.sql
+++ project_files/5_optimal_skills.sql
@@ -1,9 +1,7 @@
 /*
-Answer: What are the most optimal skills to learn (aka it’s in high demand and a high-paying skill)?
-- Identify skills in high demand and associated with high average salaries for Data Analyst roles
-- Concentrates on remote positions with specified salaries
-- Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), 
-    offering strategic insights for career development in data analysis
+Question 5 : What are the most optimal skills to learn (aka it’s in high demand and a high-paying skill)?
+- Identify skills in high demand that associated with high average salaries for Data Analyst roles
+- Why? Offer strategic insights for career development in data analysis
 */
 
 -- Identifies skills in high demand for Data Analyst roles
@@ -79,4 +77,5 @@ ORDER BY
     avg_salary DESC,
     demand_count DESC
 LIMIT 25;
+
 
